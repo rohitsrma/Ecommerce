8533b89cddd0205f22596fc1141a2df3dd3fb773
rohitsrma <rohitsrmaofficial@gmail.com>
2024-07-15 23:50:58 +0530

    use built-in login form

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
 from accounts import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/', views.login, name="login"),
+     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
